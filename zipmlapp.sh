91b1dc154fd3c0f38d4bb0083946b2a47392460f
Tom <tom@trailmarker.io>
2022-08-19 22:10:39 +0930

    updated zipmlapp.sh script to use 7-zip

--- zipmlapp.sh
+++ zipmlapp.sh
@@ -3,16 +3,16 @@
 mkdir deployment
 
 # get a clean copy of the current branch in a subdirectory `deployment`
-git archive HEAD | tar -x -C deployment
+git archive --verbose HEAD | tar -x -C deployment
 cd deployment
 
 # remove bare image files—these are encoded in resources.py anyway
 cp mlapp/images/icon.png mlapp/icon.png
 rm -rf mlapp/images
 
-# zip up the NAFI plug-in directory only into a datestamped archive
-# zip -FSr "../ntrrp-$(date +'%Y%m%d').zip" ntrrp
-# cd ..
-# rm -rf deployment
+# zip up the MLA Paddock Power plug-in directory only into a datestamped archive
+7z a -r "../mlapp-$(date +'%Y%m%d').zip" mlapp
+cd ..
+rm -rf deployment
 
 
