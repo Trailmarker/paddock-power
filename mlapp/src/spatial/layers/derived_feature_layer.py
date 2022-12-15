4f95e6e94a4577796b3404b9db178a73b058088d
Tom <tom@trailmarker.io>
2022-12-15 13:11:18 +0930

    refactor DerivedFeatureLayer to expose the collection of upstream PersistedFeatureLayers (PP-37)

--- mlapp/src/spatial/layers/derived_feature_layer.py
+++ mlapp/src/spatial/layers/derived_feature_layer.py
@@ -4,37 +4,38 @@ from urllib.parse import quote
 from qgis.core import QgsProject
 
 from .feature_layer import FeatureLayer
+from .persisted_feature_layer import PersistedFeatureLayer
 
 
 class DerivedFeatureLayer(FeatureLayer):
 
-    @staticmethod
-    def formatLayerClause(featureLayer):
-        return "layer=" + ":".join([
-            "ogr",
-            quote(featureLayer._gpkgUrl),
-            quote(featureLayer.name()),
-            "UTF-8"
-        ])
-
-    @staticmethod
-    def makeQueryClause(queryFormatSpec, *featureLayers):
-        return queryFormatSpec.format(*(layer.name() for layer in featureLayers))
-
     def __init__(self, layerName, queryFormatSpec, styleName=None, *featureLayers):
         # We don't need a layer clause for any DerivedFeatureLayers
-        layerClauses = map(
-            DerivedFeatureLayer.formatLayerClause, [
-                f for f in featureLayers if not isinstance(
-                    f, DerivedFeatureLayer)])
-        queryClause = DerivedFeatureLayer.makeQueryClause(queryFormatSpec, *featureLayers)
 
-        init = f"?{'&'.join(layerClauses)}&query={quote(queryClause)}"
+        self._featureLayers = list(featureLayers)
+        self._queryFormatSpec = queryFormatSpec
+
+        init = self._makeDerivedFeatureLayerSource()
 
         super().__init__(init, layerName, "virtual", styleName=styleName)
 
         self.detectAndRemove()
         QgsProject.instance().addMapLayer(self, False)
+
+    @property
+    def featureLayers(self):
+        """Return the feature layers that are used to derive this layer."""
+        return self._featureLayers
+
+    @property
+    def persistedFeatureLayers(self):
+        """Return the instances of Paddock Power PersistedFeatureLayers used to derive this layer."""
+        return [f for f in self.featureLayers if isinstance(f, PersistedFeatureLayer)]
+
+    @property
+    def nonDerivedFeatureLayers(self):
+        """Return the feature layers that are used to derive this layer."""
+        return [f for f in self.featureLayers if not isinstance(f, DerivedFeatureLayer)]
 
     def detectAndRemove(self):
         """Detect if a DerivedFeatureLayer is already in the map, and if so, remove it."""
@@ -42,3 +43,24 @@ class DerivedFeatureLayer(FeatureLayer):
         for layer in layers:
             if layer.name() == self.name() and layer.providerType() == "virtual":
                 QgsProject.instance().removeMapLayer(layer.id())
+
+    def _makeNonDerivedFeatureLayerParameter(self, featureLayer):
+        """Make a layer clause for a feature layer."""
+        return "layer=" + ":".join([
+            "ogr",
+            quote(featureLayer._gpkgUrl),
+            quote(featureLayer.name()),
+            "UTF-8"
+        ])
+
+    def _makeAllLayerParameters(self):
+        """Make layer clauses for all feature layers."""
+        return [self._makeNonDerivedFeatureLayerParameter(f) for f in self.nonDerivedFeatureLayers]
+
+    def _makeQueryParameter(self):
+        """Make the query parameter for a feature layer."""
+        return self._queryFormatSpec.format(*(layer.name() for layer in self.featureLayers))
+
+    def _makeDerivedFeatureLayerSource(self):
+        """Make the overall initialisation string for a DerivedFeatureLayer."""
+        return f"?{'&'.join(self._makeAllLayerParameters())}&query={quote(self._makeQueryParameter())}"
