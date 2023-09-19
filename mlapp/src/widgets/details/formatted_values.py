77d17fb464e1237e3bc9b18c4d97c1f0ff2e8d33
Tom Lynch <tom@trailmarker.io>
2023-09-19 15:05:24 +0800

    update docstring

--- mlapp/src/widgets/details/formatted_values.py
+++ mlapp/src/widgets/details/formatted_values.py
@@ -8,6 +8,7 @@ class FormattedValues(QLabel):
         super().__init__(parent)
 
     def setValues(self, formatSpec: str, *values):
+        """Format the values to be displayed in the label according to the format specification."""
         if values and all(value is not None for value in values) and formatSpec is not None:
             formattedValues = formatSpec.format(*values)
             super().setText(formattedValues)
