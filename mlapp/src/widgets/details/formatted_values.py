2d62791c18d6950d4d280c3f4b803eddf92a6a21
Tom Lynch <tom@trailmarker.io>
2023-09-19 15:04:02 +0800

    fix issue where some formatted values would be empty if any of the input values were falsy

--- mlapp/src/widgets/details/formatted_values.py
+++ mlapp/src/widgets/details/formatted_values.py
@@ -8,8 +8,9 @@ class FormattedValues(QLabel):
         super().__init__(parent)
 
     def setValues(self, formatSpec: str, *values):
-        if values and all(values) and formatSpec is not None:
-            super().setText(formatSpec.format(*values))
+        if values and all(value is not None for value in values) and formatSpec is not None:
+            formattedValues = formatSpec.format(*values)
+            super().setText(formattedValues)
         else:
             super().setText("")
         return self
